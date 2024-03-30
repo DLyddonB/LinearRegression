0142e967d10e64cfa7c59960f79987e78fab3ce2
DLyddonB <danbennett1988@hotmail.co.uk>
2024-03-30 14:14:15 +0000

    updated with comments

--- LinearRegressionExercise.py
+++ LinearRegressionExercise.py
@@ -13,13 +13,19 @@ data.describe()
 y = data['price']
 x1 = data['size']
 
+#plot scatter graph
 plt.scatter(x1, y)
 yhat = 223.1787*x1 + 101900
+# this is the regression line which is to say that it's the best fitting line, or the line which is
+# closest to all observations simultaneously
 fig = plt.plot(x1,yhat, lw=4, color='orange', label='Regression line')
 plt.xlabel('Size (m^2)', fontsize=15)
 plt.ylabel('Price ($)', fontsize=15)
 plt.show()
 
 x = sm.add_constant(x1)
+#Results containts the output of the Ordinary Least Squares (OLS) regression. As arguments we
+# need to add the dependent variable (y) and the newly defined x. fit() will apply a specific estimation technique (OLS in this case) to obtain
+# the fit of the model
 results = sm.OLS(y,x).fit()
 print(results.summary())
